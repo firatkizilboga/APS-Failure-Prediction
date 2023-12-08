f366175131590947e84268986e7ece35cc57c214
firatkizilboga <firatkizilboga11@gmail.com>
2023-12-08 15:47:18 +0300

    Initial commit

---
+++ preprocessing.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+class LDA():
+    def __init__(self) -> None:
+        pass
+
+    def fit(self, X, y) -> None:
+        pass
+
+    def transform(self, X, k_features) -> np.ndarray:
+        pass
+
+    def fit_transform(self, X, y, k_features) -> np.ndarray:
+        pass
+
+class PCA():
+    def __init__(self) -> None:
+        pass
+
+    def fit(self, X, y) -> None:
+        pass
+
+    def transform(self, X, k_features) -> np.ndarray:
+        pass
+
+    def fit_transform(self, X, y, k_features) -> np.ndarray:
+        pass
