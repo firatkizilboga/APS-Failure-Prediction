6bc0b64738065f4ce4c3fd362dec29df390f24a9
umutgokdg <umutgokdg@gmail.com>
2023-12-13 18:51:30 +0300

    pca

--- preprocessing.py
+++ preprocessing.py
@@ -1,14 +1,54 @@
 import numpy as np
 
 class LDA():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, n_components):
+        self.n_components = n_components
 
-    def fit(self, X, y) -> None:
-        pass
+    def fit(self, X, y):
+        n_components = X.shape[1]
+        class_labels = np.unique(y)
+        
+        mean_overall = np.mean(X, axis=0)
+        self.mean_ = mean_overall
+
+        S_B = np.zeros((n_components, n_components))
+        for c in class_labels:
+            X_c = X[y == c]
+            mean_c = np.mean(X_c, axis=0)
+            n_c = X_c.shape[0]
+            
+            mean_diff = (mean_c - mean_overall).reshape(n_components, 1)
+            S_B += n_c * (mean_diff @ mean_diff.T)
+        
+        # Within class scatter
+        S_W = np.zeros((n_components, n_components))
+        for c in class_labels:
+            X_c = X[y == c]
+            S_W += (X_c - mean_c).T @ (X_c - mean_c)
+
+        # Determine SW^-1 SB
+        A = np.linalg.inv(S_W) @ S_B
+        # Get eigenvalues and eigenvectors of SW^-1 SB
+        eigenvalues, eigenvectors = np.linalg.eig(A)
+        # Sort eigenvectors by eigenvalues in descending order
+        eigenvectors = eigenvectors.T
+        idxs = np.argsort(abs(eigenvalues))[::-1]
+        eigenvectors = eigenvectors[idxs]
+
+        #get the first n_components eigenvectors
+        self.L = eigenvectors[:, :self.n_components]
+
+        # compute variance explained ratio
+        self.explained_variance_ratio = np.sum(eigenvalues[:self.n_components]) / np.sum(eigenvalues)
+
+
 
     def transform(self, X, k_features) -> np.ndarray:
-        pass
+        X_centered = X - self.mean_
+        print(X_centered.shape, self.L.shape)
+        F = X_centered.dot(self.L.T)
+        return F
+        
 
     def fit_transform(self, X, y, k_features) -> np.ndarray:
         pass
@@ -20,9 +60,10 @@ class PCA():
     def fit(self, X, y) -> None:
         pass
 
-    def transform(self, X, k_features) -> np.ndarray:
+    def transform(self, X) -> np.ndarray:
+        
         pass
-
+        
     def fit_transform(self, X, y, k_features) -> np.ndarray:
         pass
 
